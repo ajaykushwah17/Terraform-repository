860a5a8d202b622adf4662585dae74d42e40ef3c
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-05 14:53:03 +0530

    Update Ec2Demo.tf

--- Ec2Demo.tf
+++ Ec2Demo.tf
@@ -1,14 +1,20 @@
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-# Define the EC2 instance resource
-resource "aws_instance" "example" {
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.10.0"
+        }
+        }
+    }
+    provider "aws" {
+        region = "ap-south-1"
+        access_key = "AKIA32BP4XBTPJT2B2MN"
+        secret_key = "AQvmskG3h5bXykOwlCJMgaoELhovS0iz1oW2bWUI"
+    }
+    resource "aws_instance" "example" {
   ami           = "ami-067c21fb1979f0b27" 
   instance_type = "t2.micro"
   tags = {
     "Name" = "Myec2Instance"
-}
-
-}
+  }
+    }
