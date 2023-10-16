e6911cdd5b128e815b1a210dd6282c705424e355
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-16 13:34:20 +0530

    Update Ec2Demo.tf

--- Ec2Demo.tf
+++ Ec2Demo.tf
@@ -1,22 +1,20 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.51.0"  # Use an appropriate version
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.10.0"
+        }
+        }
     }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-067c21fb1979f0b27"  # You can make this a variable if needed
-  instance_type = "t2.micro"  # You can make this a variable if needed
+    provider "aws" {
+        region = "ap-south-1"
+        access_key = "AKIA32BP4XBTAATMDJ47"
+        secret_key = "ihSZLkLKPm9WG1HQLMklV3frUG1Rsew/BJAQ3P6+"
+    }
+    resource "aws_instance" "example" {
+  ami           = "ami-067c21fb1979f0b27" 
+  instance_type = "t2.micro"
   tags = {
     "Name" = "Myec2Instance"
   }
-}
+    }
